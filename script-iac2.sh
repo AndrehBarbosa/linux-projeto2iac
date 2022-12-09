f35a9bbf935b8dce3c5596aeea2228ab91746e04
AndreBarbosa <andre.7lih@gmail.com>
2022-12-09 00:00:26 +0000

    apache v.1.1

---
+++ script-iac2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+echo "Baixando e copiando os arquivos da aplicação..."
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio
+cp -R * /var/www/html/
